9c9e5304d49aaf1c5321b1ca62385c21d496e535
Hossein tavalaei <greathosseinchi@gmail.com>
2024-05-16 10:06:42 +0330

    set app-component layout

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -4,11 +4,17 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatGridListModule} from '@angular/material/grid-list'
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {provideNativeDateAdapter} from '@angular/material/core'
 const modules: any = [
   MatIconModule,
   MatButtonModule,
   MatToolbarModule,
-  MatGridListModule
+  MatGridListModule,
+  MatDatepickerModule,
+  MatCardModule,
+  CommonModule
 ]
 
 @NgModule({
@@ -18,6 +24,10 @@ const modules: any = [
   ],
   exports: [
     ...modules
+  ],
+
+  providers: [
+    provideNativeDateAdapter()
   ]
 })
 export class MaterialModule { }
--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-const components: any = [];
+const components: any = [
+
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    
+  ],
   imports: [
     ...components
   ],
